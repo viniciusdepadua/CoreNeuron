326c07e0a6584671a9607913380ba4add5aaf832
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2021-05-11 10:50:04 +0200

    Update solve_core.cpp

--- coreneuron/sim/solve_core.cpp
+++ coreneuron/sim/solve_core.cpp
@@ -16,16 +16,10 @@ static void triang(NrnThread*), bksub(NrnThread*);
 
 /* solve the matrix equation */
 void nrn_solve_minimal(NrnThread* _nt) {
-    if (use_solve_interleave) {
-        solve_interleaved(_nt->id);
-    } else {
         triang(_nt);
         bksub(_nt);
     }
 }
-
-/** TODO loops are executed seq in OpenACC just for debugging, remove it! */
-
 /* triangularization of the matrix equations */
 static void triang(NrnThread* _nt) {
     int i2 = _nt->ncell;
@@ -37,17 +31,9 @@ static void triang(NrnThread* _nt) {
     double* vec_rhs = &(VEC_RHS(0));
     int* parent_index = _nt->_v_parent_index;
 
-#if defined(_OPENACC)
-    int stream_id = _nt->stream_id;
-#endif
-    /** @todo: just for benchmarking, otherwise produces wrong results */
-    // clang-format off
-
     #pragma acc parallel loop seq present(      \
         vec_a[0:i3], vec_b[0:i3], vec_d[0:i3],  \
-        vec_rhs[0:i3], parent_index[0:i3])      \
-        async(stream_id) if (_nt->compute_gpu)
-    // clang-format on
+        vec_rhs[0:i3], parent_index[0:i3])
     for (int i = i3 - 1; i >= i2; --i) {
         double p = vec_a[i] / vec_d[i];
         vec_d[parent_index[i]] -= p * vec_b[i];
@@ -66,22 +52,11 @@ static void bksub(NrnThread* _nt) {
     double* vec_rhs = &(VEC_RHS(0));
     int* parent_index = _nt->_v_parent_index;
 
-#if defined(_OPENACC)
-    int stream_id = _nt->stream_id;
-#endif
-    /** @todo: just for benchmarking, otherwise produces wrong results */
-    // clang-format off
-
     #pragma acc parallel loop seq present(      \
-        vec_d[0:i2], vec_rhs[0:i2])             \
-        async(stream_id) if (_nt->compute_gpu)
-    // clang-format on
+        vec_d[0:i2], vec_rhs[0:i2])            
     for (int i = i1; i < i2; ++i) {
         vec_rhs[i] /= vec_d[i];
     }
-
-    /** @todo: just for benchmarking, otherwise produces wrong results */
-    // clang-format off
 
     #pragma acc parallel loop seq present(          \
         vec_b[0:i3], vec_d[0:i3], vec_rhs[0:i3],    \
@@ -91,8 +66,5 @@ static void bksub(NrnThread* _nt) {
         vec_rhs[i] -= vec_b[i] * vec_rhs[parent_index[i]];
         vec_rhs[i] /= vec_d[i];
     }
-
-    #pragma acc wait(stream_id)
-    // clang-format on
 }
 }  // namespace coreneuron
